911f8f5cab0b9fc7fafd99fe758f7bc3a3226940
Tero Marttila <terom@fixme.fi>
2015-01-29 20:34:51 +0200

    qmsk-e2: qmsk.e2.client: --preset-recall and --cut/--autotrans/--trans FRAMES
    --HG--
    extra : convert_revision : c52b3dbbe9efe2bd2e4866a54ff6764bf19c847e

---
+++ qmsk/e2/client.py
@@ -0,0 +1,89 @@
+import asyncio
+import logging; log = logging.getLogger('qmsk.e2.client')
+import qmsk.net.tcp
+
+class Error(Exception):
+    pass
+
+class CommandError(Error):
+    pass
+
+class E2Client:
+    PORT = 9878
+
+    @classmethod
+    @asyncio.coroutine
+    def connect (cls, host, port=PORT):
+        """
+            Raises qmsk.net.tcp.Error
+        """
+
+        stream = yield from qmsk.net.tcp.connect(host, port)
+        
+        log.info("%s: connected: %s", host, stream)
+
+        return cls(stream)
+
+    def __init__ (self, stream):
+        self.stream = stream
+
+    @asyncio.coroutine
+    def cmd (self, cmd, *args):
+        """
+            Raises qmsk.net.tcp.Error, CommandError
+        """
+        line = ' '.join([cmd] + [str(arg) for arg in args])
+
+        log.info("%s: %s", self, line)
+        
+        yield from self.stream.writeline(line)
+
+        while True:
+            line = yield from self.stream.readline()
+
+            log.debug("%s: %s: %r", self, cmd, line)
+
+            if line.startswith('\x04'):
+                # wtf
+                line = line[1:]
+
+            parts = line.split()
+
+            if not parts:
+                # skip
+                continue
+
+            elif len(parts) == 3 and parts[0] == cmd and parts[1] == '-e':
+                try:
+                    err = int(parts[2])
+                except ValueError as error:
+                    raise CommandError("%s: invalid error status: %s: %s", cmd, parts[2], line)
+
+                break
+            else:
+                log.warning("%s: %s: %r", self, cmd, line)
+           
+        if err:
+            raise CommandError(cmd, err)
+
+    @asyncio.coroutine
+    def PRESET_recall (self, preset):
+        """
+            preset:int      0-1000 
+        """
+
+        yield from self.cmd('PRESET', '-r', preset)
+
+    @asyncio.coroutine
+    def ATRN (self, transTime=None):
+        """
+            transTime:int   frames
+        """
+        
+        if transTime is None:
+            yield from self.cmd('ATRN')
+        else:
+            yield from self.cmd('ATRN', transTime)
+
+    def __str__ (self):
+        return str(self.stream)
