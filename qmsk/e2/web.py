410a22c3f07c65cf872817b177d33ee96f266741
Tero Marttila <terom@fixme.fi>
2015-01-29 22:49:06 +0200

    qmsk.e2.web: json api and presets
    --HG--
    extra : convert_revision : 7a0bd0c4efaada9b11df5aa399997ba18b2c5108

--- qmsk/e2/web.py
+++ qmsk/e2/web.py
@@ -6,13 +6,12 @@ import werkzeug
 
 import qmsk.web.async
 import qmsk.web.html
+import qmsk.web.json
 import qmsk.web.urls
 
 html = qmsk.web.html.html5
 
-class Index(qmsk.web.html.HTMLMixin, qmsk.web.async.Handler):
-    TITLE = "Hello World!!!!"
-    
+class BaseHandler(qmsk.web.async.Handler):
     def init(self):
         self.preset = self.autotrans = self.error = None
 
@@ -20,28 +19,62 @@ class Index(qmsk.web.html.HTMLMixin, qmsk.web.async.Handler):
     def process_async(self):
         if self.request.method == 'POST':
             self.preset, self.autotrans, self.error = yield from self.app.process(self.request.form)
-        
+ 
+class Index(qmsk.web.html.HTMLMixin, BaseHandler):
+    TITLE = "Encore2 Control"
+       
+
+    def render_preset(self, preset):
+        return html.button(type='submit', name='preset', value=preset.preset)(preset.title)
+
     def render(self):
         return (
             html.h1(self.title()),
-            html.p("Recalled preset {preset}".format(preset=self.preset)) if self.preset is not None else None,
-            html.p("Autotransitioned {autotrans}".format(autotrans=self.autotrans)) if self.autotrans is not None else None,
-            html.p("Error: {error}".format(error=self.error)) if self.error else None,
             html.form(action='', method='POST')(
-                html.input(type='text', name='preset', placeholder='Preset number'),
-                html.input(type='submit', name='cut', value='Cut'),
-                html.input(type='submit', name='autotrans', value='Auto Trans'),
-            )
+                html.div(
+                    self.render_preset(preset) for preset in self.app.presets
+                ),
+                html.div(
+                    html.input(type='submit', name='cut', value='Cut'),
+                    html.input(type='submit', name='autotrans', value='Auto Trans'),
+                )
+            ),
+            html.div(
+                html.p("Recalled preset {preset}".format(preset=self.preset)) if self.preset is not None else None,
+                html.p("Autotransitioned {autotrans}".format(autotrans=self.autotrans)) if self.autotrans is not None else None,
+                html.p("Error: {error}".format(error=self.error)) if self.error else None,
+            ),
         )
 
+class API(qmsk.web.json.JSONMixin, BaseHandler):
+    def render_json(self):
+        out = dict()
+        
+        if self.preset is not None:
+            out['preset'] = self.preset
+
+        if self.autotrans is not None:
+            out['autotrans'] = self.autotrans
+
+        if self.error is not None:
+            out['error'] = self.error
+
+        return out
+
 class E2Web(qmsk.web.async.Application):
-    def __init__ (self, client):
+    URLS = qmsk.web.urls.rules({
+        '/':        Index,
+        '/api/v1':  API,
+    })
+
+    def __init__ (self, client, presets):
         """
             client: qmsk.e2.client.E2Client
         """
         super().__init__()
 
         self.client = client
+        self.presets = presets
     
     @asyncio.coroutine
     def process(self, params):
@@ -86,17 +119,13 @@ class E2Web(qmsk.web.async.Application):
         else:
             return preset, autotrans, None
 
-    URLS = qmsk.web.urls.rules({
-        '/': Index,
-    })
-
 @asyncio.coroutine
-def start (loop, client, port, host=None):
+def start (client, presets, loop, port, host=None):
     """
         client: qmsk.e2.client.E2Client
     """
 
-    application = E2Web(client)
+    application = E2Web(client, presets)
 
     def server_factory():
         return aiohttp.wsgi.WSGIServerHttpProtocol(application,
