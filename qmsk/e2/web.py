57952fcf296ae91ad9d6a492df0855999d9f56d9
Tero Marttila <terom@paivola.fi>
2015-01-30 19:11:50 +0200

    qmsk.web: html header with nav links

--- qmsk/e2/web.py
+++ qmsk/e2/web.py
@@ -66,8 +66,42 @@ class HTMLBase(qmsk.web.html.HTMLMixin, BaseHandler):
         else:
             return 200
 
+    def render_header(self):
+        return html.div(id='header', class_='navbar')(
+            html.div(class_='navbar-header')(
+                html.a(href='#', class_='navbar-brand')(self.TITLE),
+            ),
+            html.div(class_='narbar-collapse')(
+                html.ul(class_='nav navbar-nav')(
+                    html.li(class_=('active' if isinstance(self, page) else None))(
+                        html.a(href=self.url(page))(page.PAGE_TITLE)
+                    ) for page in HTML_PAGES
+                ),
+            ),
+        )
+
+    def render_status(self):
+        return [ ]
+
+    def render_content(self):
+        raise NotImplementedError()
+
+    def render(self):
+        status = []
+
+        for msg in self.render_status():
+            status.append(html.p(msg))
+
+        return html.div(class_='container-fluid', id='container')(
+            self.render_header(),
+            self.render_content(),
+            html.div(id='status')(
+                status or html.p("Ready")
+            ),
+        )
+
 class HTMLPresets(HTMLBase):
-    TITLE = "Encore2 Control - Presets"
+    PAGE_TITLE = "Presets"
 
     def render_preset_destination(self, preset, destination):
         if preset == destination.program:
@@ -113,42 +147,30 @@ class HTMLPresets(HTMLBase):
                     self.render_preset(preset) for preset in group.presets
                 ],
         )
-
-    def render(self):
-        status = []
-
+    def render_status(self):
         for value, message in (
                 (self.preset, "Recalled preset {}"),
                 (self.transition, "Transitioned {}"),
                 (self.error, "Error: {}")
         ):
             if value is not None:
-                status.append(html.p(message.format(value)))
+                yield message.format(value)
 
-        return html.div(class_='container-fluid', id='container')(
+    def render_content(self):
+        return html.form(action='', method='POST')(
             html.div(
-                html.div(id='header')(
-                    html.h1(self.title()),
+                html.div(id='tools')(
+                    html.button(type='submit', name='cut', value='cut', id='cut')("Cut"),
+                    html.button(type='submit', name='autotrans', value='autotrans', id='autotrans')("Auto Trans"),
                 ),
-            ),
-            html.form(action='', method='POST')(
-                html.div(
-                    html.div(id='tools')(
-                        html.button(type='submit', name='cut', value='cut', id='cut')("Cut"),
-                        html.button(type='submit', name='autotrans', value='autotrans', id='autotrans')("Auto Trans"),
-                    ),
-                    html.div(id='presets', class_='presets')(
-                        self.render_preset_group(group) for group in self.app.presets.groups
-                    ),
-                ),
-                html.div(id='status')(
-                    status or html.p("Ready")
+                html.div(id='presets', class_='presets')(
+                    self.render_preset_group(group) for group in self.app.presets.groups
                 ),
             ),
         )
 
 class HTMLDestinations(HTMLBase):
-    TITLE = "Encore2 Control - Destinations"
+    PAGE_TITLE = "Destinations"
 
     def render_destination_preset(self, preset, class_):
         css = set(['preset'])
@@ -169,19 +191,17 @@ class HTMLDestinations(HTMLBase):
                 self.render_destination_preset(destination.preview, 'preview'),
         )
 
-    def render(self):
-        return html.div(class_='container-fluid', id='container')(
-            html.div(
-                html.div(id='header')(
-                    html.h1(self.title()),
-                ),
-            ),
-            html.div(
-                html.div(id='destinations', class_='presets')(
-                    self.render_destination(destination) for destination in self.app.presets.destinations
-                ),
+    def render_content(self):
+        return html.div(
+            html.div(id='destinations', class_='presets')(
+                self.render_destination(destination) for destination in self.app.presets.destinations
             ),
         )
+
+HTML_PAGES = (
+    HTMLPresets,
+    HTMLDestinations,
+)
 
 class APIBase (qmsk.web.json.JSONMixin, qmsk.web.async.Handler):
     CORS_ORIGIN = '*'
