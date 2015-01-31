f68b6db944a31cba5830fd472458d5665f6c9b2f
Arto Seppa <zar@iki.fi>
2015-01-31 14:38:33 +0200

    Add seq handling and visible logging

--- static/client/app.js
+++ static/client/app.js
@@ -6,6 +6,13 @@ phonecatApp.controller('PresetsCtrl', function ($scope, $http, $websocket) {
 
 	$scope.base = {};
 	$scope.base.inPreview = null;
+	$scope.status = [];
+	$scope.seq = 0;
+
+	$scope.log = function (msg, data) {
+		console.log(msg, data);
+		$scope.status.unshift({msg: msg, data: data});
+	};
 
 	var ws = $websocket.$new({
 		url: 'ws://192.168.0.16:8082',
@@ -13,54 +20,60 @@ phonecatApp.controller('PresetsCtrl', function ($scope, $http, $websocket) {
 	});
 
     ws.$on('$open', function () {
-        console.log('websocket opened');
+        $scope.log('websocket opened');
         ws.$emit('ping', 'hello');
+        $scope.loadPresets(); // reload to get current seq
     });
 
     ws.$on('$message', function (data) {
-        console.log('websocket data:', data);
+        $scope.log('websocket data: ', data);
         $scope.loadPresets();
     });
 
     ws.$on('$close', function () {
-        console.log('websocket closed');
+        $scope.log('websocket closed');
     });
+
+    var fff = false;
 
 	$scope.loadPresets = function() {
 		$http.get(backendUrl)
 			.success(function(data) {
 				$scope.data = data;
+				$scope.seq = data.seq;
 			}).error(function(err) {
-				console.log("error", err);
+				$scope.log('error loading preset data');
 			});
 	};
 	$scope.loadPresets();
 
 
 	$scope.clickPreset = function(id) {
-		$http.post(backendUrl + 'preset/' + id)
+		$http.post(backendUrl + 'preset/' + id, {seq: $scope.seq})
 			.success(function(data) {
-				// nothing
+				$scope.log('preset clicked:', data);
 			}).error(function(err) {
-				console.log("error", err);
+				$scope.log('error when selecting preset');
+				$scope.loadPresets(); // reload to get current seq
 			});
 		return false;
 	};
 
 	$scope.autotrans = function() {
-		return $scope.setInPgm({autotrans: true});
+		return $scope.setInPgm({autotrans: true, seq: $scope.seq});
 	}
 
 	$scope.cut = function() {
-		return $scope.setInPgm({cut: true});
+		return $scope.setInPgm({cut: true, seq: $scope.seq});
 	}
 
 	$scope.setInPgm = function(data) {
 		$http.post(backendUrl + 'preset/', data)
 			.success(function(data) {
-				// nothing
+				$scope.log('transition clicked: ', data);
 			}).error(function(err) {
-				console.log("error", err);
+				$scope.log('error with transition');
+				$scope.loadPresets(); // reload to get current seq
 			});
 		return false;
 	}
