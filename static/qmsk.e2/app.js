c98168c92a61b2e4fd68383f228b7802c06d34b5
Tero Marttila <terom@fixme.fi>
2016-02-06 16:20:38 +0200

    web: main ordering

--- static/qmsk.e2/app.js
+++ static/qmsk.e2/app.js
@@ -11,6 +11,7 @@ angular.module('qmsk.e2', [
         .when('/main', {
             templateUrl: 'qmsk.e2/main.html',
             controller: 'MainCtrl',
+            reloadOnSearch: false,
         })
         .when('/sources', {
             templateUrl: 'qmsk.e2/sources.html',
@@ -89,9 +90,31 @@ angular.module('qmsk.e2', [
     };
 })
 
-.controller('MainCtrl', function($scope, Index) {
+.controller('MainCtrl', function($scope, $location, Index) {
+    $scope.selectOrder = function(order) {
+        $scope.order = order;
+        $scope.orderBy = function(){
+            switch (order) {
+            case 'source':
+                return ['-type', 'name'];
+            case 'preview':
+                return ['preview_screens'];
+            case 'program':
+                return ['program_screens'];
+            default:
+                return [];
+            }
+        }();
+
+        $location.search('order', order);
+    };
+    $scope.selectOrder($location.search().order);
+
     Index().then(function success(index) {
-        $scope.index = index;
+        $scope.screens = index.screens
+        $scope.sources = $.map(index.sources, function(source, id){
+            return source;
+        });
     });
 })
 
