b494003f6132744a8d165cd12d5034807bdc8d5a
Tero Marttila <terom@fixme.fi>
2016-02-09 09:50:38 +0200

    client: dump-config from .xml file

---
+++ client/config.go
@@ -0,0 +1,16 @@
+package client
+
+import (
+    "os"
+    "encoding/xml"
+)
+
+func LoadSettingsFile(path string) (system System, err error) {
+    if file, err := os.Open(path); err != nil {
+        return system, err
+    } else if err := xml.NewDecoder(file).Decode(&system); err != nil {
+        return system, err
+    } else {
+        return system, nil
+    }
+}
---
+++ cmd/client/config_dump.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "github.com/qmsk/e2/client"
+    "log"
+    "os"
+)
+
+type ConfigDump struct {
+    SettingsXML     string      `long:"settings-xml" value-name:"PATH"`
+}
+
+func init() {
+    parser.AddCommand("config-dump", "Dump XML config", "", &ConfigDump{})
+}
+
+func (cmd *ConfigDump) Execute(args []string) error {
+    if system, err := client.LoadSettingsFile(cmd.SettingsXML); err != nil {
+        return err
+    } else {
+        log.Printf("Loaded: %v\n", cmd.SettingsXML)
+
+        system.Print(os.Stdout)
+    }
+
+    return nil
+}
