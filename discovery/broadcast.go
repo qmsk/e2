ce6fb8d221e1bc4b1aa33cb3865c1467848c7345
Tero Marttila <terom@fixme.fi>
2016-06-22 21:54:50 +0300

    discovery: test and correctly handle IPv4-only aspect of lookupInterfaceBroadcast

--- discovery/broadcast.go
+++ discovery/broadcast.go
@@ -6,44 +6,54 @@ import (
     "net"
 )
 
-// Calculate the interface broadcast address from the interface unicast address
+// Calculate the IPv4 broadcast address from the interface unicast address
 func broadcastIP(addr *net.IPNet) (net.IP, error) {
-    ip4 := addr.IP.To4()
-    bits, size := addr.Mask.Size()
+    if ip4 := addr.IP.To4(); ip4 != nil {
+        bits, size := addr.Mask.Size()
 
-    if size != 32 || ip4 == nil {
-        return nil, fmt.Errorf("Invalid IPv4 address: %v", addr)
+        hostBits := uint(size - bits)
+        net := binary.BigEndian.Uint32([]byte(ip4))
+        bcast := net | ((1 << hostBits) - 1)
+
+        binary.BigEndian.PutUint32([]byte(ip4), bcast)
+
+        return ip4, nil
+    } else {
+        // skip
+        return nil, nil
     }
-
-    hostBits := uint(size - bits)
-    net := binary.BigEndian.Uint32([]byte(ip4))
-    bcast := net | ((1 << hostBits) - 1)
-
-    binary.BigEndian.PutUint32([]byte(ip4), bcast)
-
-    return ip4, nil
 }
 
-// Return first broadcast address for interface
+func getInterfaceBroadcast(iface *net.Interface, addrs []net.Addr) (net.IP, error) {
+    if iface.Flags & net.FlagUp == 0 {
+        return nil, fmt.Errorf("Interface is down: %v", iface.Name)
+    } else if iface.Flags & net.FlagBroadcast == 0 {
+        return nil, fmt.Errorf("Interface is not broadcast: %v", iface.Name)
+    }
+
+    for _, ifaceAddr := range addrs {
+        switch addr := ifaceAddr.(type) {
+        case *net.IPNet:
+            if ip, err := broadcastIP(addr); err != nil {
+                return nil, err
+            } else if ip != nil {
+                return ip, nil
+            } else {
+                // skip, probably IPv6
+            }
+        }
+    }
+
+    return nil, fmt.Errorf("No broadcast address for interface: %v", iface.Name)
+}
+
+// Return first IPv4 broadcast address for named interface
 func lookupInterfaceBroadcast(name string) (net.IP, error) {
     if iface, err := net.InterfaceByName(name); err != nil {
         return nil, err
-    } else if iface.Flags & net.FlagUp == 0 {
-        return nil, fmt.Errorf("Interface is down: %v", name)
-    } else if iface.Flags & net.FlagBroadcast == 0 {
-        return nil, fmt.Errorf("Interface is not broadcast: %v", name)
+    } else if addrs, err := iface.Addrs(); err != nil {
+        return nil, err
     } else {
-        if addrs, err := iface.Addrs(); err != nil {
-            return nil, err
-        } else {
-            for _, ifaceAddr := range addrs {
-                switch addr := ifaceAddr.(type) {
-                case *net.IPNet:
-                    return broadcastIP(addr)
-                }
-            }
-        }
-
-        return nil, fmt.Errorf("No broadcast address for interface: %v", name)
+        return getInterfaceBroadcast(iface, addrs)
     }
 }
--- discovery/broadcast_test.go
+++ discovery/broadcast_test.go
@@ -1,6 +1,7 @@
 package discovery
 
 import (
+    "fmt"
     "net"
     "strings"
     "testing"
@@ -18,7 +19,6 @@ func TestBroadcastIP(t *testing.T) {
         },
         {
             cidr:  "fe80::5544:33ff:fe22:1100/64",
-            err:   "Invalid IPv4 address: ",
         },
     }
 
@@ -36,8 +36,106 @@ func TestBroadcastIP(t *testing.T) {
             } else if !strings.HasPrefix(err.Error(), test.err) {
                 t.Errorf("broadcastIP %v: error mismatch: %v", ipnet, err)
             }
-        } else if bcast.String() != test.bcast {
+        } else if test.bcast == "" && bcast == nil {
+            t.Logf("broadcastIP %v: ok: nil", ipnet)
+        } else if bcast.String() == test.bcast {
+            t.Logf("broadcastIP %v: ok: %v", ipnet, bcast)
+        } else {
             t.Errorf("broadcastIP %v: mismatch: %v", ipnet, bcast)
+        }
+    }
+}
+
+// XXX: this test assume the system will have some interfaces configured with broadcast addresses...
+// XXX: would probably be better to test using fake Interface structs...
+/* 
+    if addrs, err := iface.Addrs(); err != nil || len(addrs) == 0 {
+        panic(err)
+    } else {
+        t.Logf("Interface %v: %#v", iface.Name, iface)
+
+        for _, addr := range addrs {
+            t.Logf("\t%#v", addr)
+        }
+    } 
+*/
+func TestGetInterfaceBroadcast(t *testing.T) {
+    var testInterfaceBroadcast = []struct{
+        iface   *net.Interface
+        addrs   []net.Addr
+        bcast   string
+        err     string
+    } {
+        {
+            iface: &net.Interface{
+                Index: 1,
+                MTU:   1500,
+                Name: "test-down",
+                HardwareAddr: net.HardwareAddr{0x5, 0x44, 0x33, 0x22, 0x11, 0x00},
+                Flags: (net.FlagMulticast | net.FlagBroadcast),
+            },
+            err: "Interface is down: test-down",
+        },
+        {
+            iface: &net.Interface{
+                Index: 1,
+                MTU:   1500,
+                Name: "test-empty",
+                HardwareAddr: net.HardwareAddr{0x5, 0x44, 0x33, 0x22, 0x11, 0x00},
+                Flags: (net.FlagMulticast | net.FlagBroadcast | net.FlagUp),  // 0x13
+            },
+            addrs: []net.Addr{
+
+            },
+            err: "No broadcast address for interface: test-empty",
+        },
+        {
+            iface: &net.Interface{
+                Index: 1,
+                MTU:   1500,
+                Name: "test-basic",
+                HardwareAddr: net.HardwareAddr{0x5, 0x44, 0x33, 0x22, 0x11, 0x00},
+                Flags: (net.FlagMulticast | net.FlagBroadcast | net.FlagUp),  // 0x13
+            },
+            addrs: []net.Addr{
+                // 192.168.1.10/24
+                &net.IPNet{IP:net.IP{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xff, 0xff, 0xc0, 0xa8, 0x1, 0xa}, Mask:net.IPMask{0xff, 0xff, 0xff, 0x0}},
+                // fe80::5544:33ff:fe22:1100/64
+                &net.IPNet{IP:net.IP{0xfe, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x55, 0x44, 0x33, 0xff, 0xfe, 0x22, 0x11, 0x00}, Mask:net.IPMask{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}},
+            },
+            bcast: "192.168.1.255",
+        },
+        {
+            iface: &net.Interface{
+                Index: 1,
+                MTU:   1500,
+                Name: "test-v6only",
+                HardwareAddr: net.HardwareAddr{0x5, 0x44, 0x33, 0x22, 0x11, 0x00},
+                Flags: (net.FlagMulticast | net.FlagBroadcast | net.FlagUp),  // 0x13
+            },
+            addrs: []net.Addr{
+                // fe80::5544:33ff:fe22:1100/64
+                &net.IPNet{IP:net.IP{0xfe, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x55, 0x44, 0x33, 0xff, 0xfe, 0x22, 0x11, 0x00}, Mask:net.IPMask{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}},
+            },
+            err: "No broadcast address for interface: test-v6only",
+        },
+    }
+
+    for _, test := range testInterfaceBroadcast {
+        if bcast, err := getInterfaceBroadcast(test.iface, test.addrs); err != nil {
+            if test.err == "" {
+                t.Errorf("ERR getInterfaceBroadcast %v: error: %v", test.iface.Name, err)
+            } else if !strings.HasPrefix(err.Error(), test.err) {
+                t.Errorf("ERR getInterfaceBroadcast %v: error: %v", test.iface.Name, err)
+            } else {
+                t.Logf("OK  getInterfaceBroadcast %v: error: %v", test.iface.Name, err)
+            }
+        } else if bcast == nil && test.bcast == "" {
+            t.Logf("OK  getInterfaceBroadcast %v: %v", test.iface.Name, bcast)
+        } else if fmt.Sprintf("%v", bcast) != test.bcast {
+            t.Errorf("ERR getInterfaceBroadcast %v: %v", test.iface.Name, bcast)
+        } else {
+            t.Logf("OK  getInterfaceBroadcast %v: %v", test.iface.Name, bcast)
         }
     }
 }
