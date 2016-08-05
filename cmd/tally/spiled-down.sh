6e05dc3a3975f2d8792fe5e95b819db0fe02b19f
Tero Marttila <terom@fixme.fi>
2016-08-05 14:53:35 +0300

    cmd/tally/spiled-down.sh: shutdown SPI-LED output

---
+++ cmd/tally/spiled-down.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Trvial bash script to shutdown SPI-LED output.
+#
+# Used for e.g. systemd [Service] ExecStopPost=...
+
+count=$1
+DEVICE=${2:-/dev/spidev0.0}
+
+(
+    # start frame
+    printf '\0\0\0\0'
+
+    # led frames
+    for i in $(seq $count); do
+        printf '\xC0\x00\x00\x00'
+    done
+    
+    # stop frame
+    printf '\0\0\0\0\0\0\0\0'
+
+) > $DEVICE
+
