34829ae2453b625c012d8f39d8b34ce7d22da12c
Tero Marttila <terom@fixme.fi>
2016-08-06 12:52:49 +0300

    client/test-xml: stream.sh optional save to file

--- client/test-xml/stream.sh
+++ client/test-xml/stream.sh
@@ -1,6 +1,12 @@
 set -ue
 
 target=$1
-stream=${2:-stream-$(date +%Y%m%d-%H%M%S).xml}
+stream=${2:-}
 
-cat send-reset.xml | nc -q -1 -v $target 9876 | tee -a $stream
+cat send-reset.xml | nc -q -1 -v $target 9876 | (
+    if [ -n "$stream" ]; then
+        tee -a $stream
+    else
+        cat
+    fi
+)
