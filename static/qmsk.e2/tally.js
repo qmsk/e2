afed35902d4594e89d6501b8b8c8f05c103cbf19
Tero Marttila <terom@fixme.fi>
2016-08-06 14:06:04 +0300

    tally: use a single websocket for the entire UI, including sources, tally, inputs and outputs

--- static/qmsk.e2/tally.js
+++ static/qmsk.e2/tally.js
@@ -20,17 +20,20 @@ angular.module('qmsk.e2.tally', [
             controller: 'TallyCtrl',
             reloadOnSearch: false,
         })
+        .when('/inputs', {
+            templateUrl: '/static/qmsk.e2/tally-inputs.html',
+            controller: 'InputsCtrl',
+            reloadOnSearch: false,
+        })
+        .when('/outputs', {
+            templateUrl: '/static/qmsk.e2/tally-outputs.html',
+            controller: 'OutputsCtrl',
+            reloadOnSearch: false,
+        })
+
         .otherwise({
             redirectTo: '/tally',
         });
-})
-
-.factory('Sources', function($http) {
-    return $http.get('/api/sources').then(
-        function success(r) {
-            return r.data;
-        }
-    );
 })
 
 .factory('Tally', function($http) {
@@ -49,16 +52,25 @@ angular.module('qmsk.e2.tally', [
     };
 })
 
-.controller('SourcesCtrl', function($scope, Sources) {
-    Sources.then(function(sources){
-        $scope.sources = sources;
-    });
-})
-
-.controller('TallyCtrl', function($scope, Events) {
+.controller('StateCtrl', function($scope, Events) {
     $scope.tally = Events.state.tally;
 
     $scope.$on('qmsk.e2.event', function($e, event){
         $scope.tally = Events.state.tally;
     });
 })
+
+.controller('TallyCtrl', function($scope) {
+
+})
+.controller('SourcesCtrl', function($scope) {
+
+})
+.controller('InputsCtrl', function($scope) {
+
+})
+.controller('OutputsCtrl', function($scope) {
+
+})
+
+;
